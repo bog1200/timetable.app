f9958baceb8267cf08dc4c9e7ff6b51e24a19846
Bogdan Stefanescu <26150024+bog1200@users.noreply.github.com>
2024-08-08 11:11:22 +0300

    Auth test 5

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -1,7 +1,7 @@
 
 'use server'
 
-import { signIn, signOut } from "@/app/auth/auth";
+import { signIn, signOut } from "@/auth";
 
 export async function doSocialLogin(formData:any) {
     const action = formData.get('action');
